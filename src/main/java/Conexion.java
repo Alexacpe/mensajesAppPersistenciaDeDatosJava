ca3b6909694dfdbbf7b61827afa70335a9ae5e6b
Alex Acevedo <alex.acpe@outlook.com>
2022-07-05 22:56:05 -0500

    Creacion de clase con metodo para la conexion a la base de datos

---
+++ src/main/java/Conexion.java
@@ -0,0 +1,21 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+    //metodo con las herramientas de conexion
+    public Connection get_connection(){
+        Connection connection = null;
+        try {
+            //los parametros que se reciben son los datos de conexion a la base de datos
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mensajes_app","root","");
+            if (connection != null){
+                System.out.println("Conexion a la base de datos fue exitosa");
+            }
+            //catch es para manejar la excepcion si no se pudo conectar para enviar el mensaje de error
+        }catch (SQLException e){
+            System.out.println(e);
+        }
+        return connection;//retornar el objeto de Tipo Connection
+    }
+}
